c10e758679458a74ad759aa8e9e3a5cadad19ebb
Qingrong Ke <keqingrong1992@gmail.com>
2020-11-19 17:14:27 +0800

    Add TypeScript declaration file

--- src/stringify-array.js
+++ src/stringify-array.js
@@ -5,8 +5,8 @@ import { parseArray } from './parse-array';
 
 /**
  * 序列化数组，undefined, null, '' 都转换为空字符串（未定义），而不是 '[]'
- * @param {Array} value
- * @returns {*}
+ * @param {*} value
+ * @returns {string}
  */
 function stringifyArray(value) {
   const arr = parseArray(value);
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+export function parseArray<T>(value?: any): Array<T> | null;
+export function parseBoolean(value?: any): boolean;
+export function parseInt(string: string, radix?: number): number;
+export function parseFloat(string: string): number;
+export function parseObject<T>(value?: any): T | null;
+export function stringifyArray(value?: any): string;
+export function stringifyObject(value?: any): string;
